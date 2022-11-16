da0fdb1dd56ee0cfb7e7107cfb443c68b4b4e315
Alex K <smileyman5@twcny.rr.com>
2022-11-15 20:58:00 -1000

    Initial commit for Rust 'Get Started'

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Welcome to the life of Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    ferris_says::say(message.as_bytes(), width, &mut writer).unwrap();
+}
